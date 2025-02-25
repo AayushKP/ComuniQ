b8a46ade6eff6e40fc5c757b13fded9051f58f19
Ayush Kashyap <ayushprokashyap@gmail.com>
2025-02-25 20:49:44 +0530

    responsiveness added

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,21 +5,21 @@ import { useNavigate } from "react-router-dom";
 export default function Home() {
   const navigate = useNavigate();
   return (
-    <div className=" bg-gradient-to-br from-gray-800 to-blue-950 text-white min-h-screen flex flex-col items-center justify-center p-10">
-      <div className="flex flex-col md:flex-row items-center justify-evenly w-full max-w-6xl">
+    <div className="bg-gradient-to-br from-gray-800 to-blue-950 text-white min-h-screen flex flex-col items-center justify-center p-8">
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-evenly w-full max-w-6xl">
         <motion.div
           initial={{ opacity: 0, x: -50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8 }}
-          className="max-w-lg text-center md:text-left"
+          className="max-w-lg text-center md:text-left order-2 md:order-1"
         >
-          <h1 className="text-6xl font-bold bg-clip-text font-poppins text-transparent bg-gradient-to-r from-red-600 to-yellow-400">
+          <h1 className="text-4xl md:text-6xl font-bold bg-clip-text font-poppins text-transparent bg-gradient-to-r from-red-600 to-yellow-400">
             ComuniQ
           </h1>
-          <p className="text-2xl text-gray-400 mt-4">
+          <p className="text-xl text-gray-400 mt-2">
             Seamless real-time communication, redefined.
           </p>
-          <p className="text-lg text-gray-400 mt-6">
+          <p className="text-lg text-gray-400 mt-2 hidden md:block">
             Connect with your team, friends, and family effortlessly. Experience
             the future of communication today.
           </p>
@@ -41,7 +41,7 @@ export default function Home() {
           initial={{ opacity: 0, x: 50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8 }}
-          className="mt-10 md:mt-0"
+          className=" md:mt-0 order-1 md:order-2"
         >
           <img
             src="https://illustrations.popsy.co/amber/communication.svg"
@@ -56,7 +56,7 @@ export default function Home() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 1, duration: 0.8 }}
-        className="mt-20 text-center text-gray-400"
+        className="mt-14 text-center text-gray-400"
       >
         <p>© 2025 ComuniQ. All rights reserved.</p>
       </motion.div>
