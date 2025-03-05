180a004dec63c1b66eea769e262bd9e4da1a054b
Ayush Kashyap <ayushprokashyap@gmail.com>
2025-03-06 00:07:08 +0530

    Googlecontroller added

---
+++ server/controllers/GoogleAuthController.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const googleAuthCallback = (req, res) => {
+  const user = req.user;
+
+  const token = jwt.sign(
+    { email: user.email, id: user.id },
+    "your-secret-key",
+    { expiresIn: "3d" }
+  );
+
+  res.cookie("jwt", token, {
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+    secure: true,
+    sameSite: "None",
+  });
+
+  res.redirect(process.env.CLIENT_REDIRECT_URL || "http://localhost:3000/chat");
+};
