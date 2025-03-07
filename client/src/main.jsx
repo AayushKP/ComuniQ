39a7917b3de692613467ba4d8d721e804a8824cc
Ayush Kashyap <ayushprokashyap@gmail.com>
2025-03-07 19:39:59 +0530

    vercel-analytics added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "sonner";
 import { SocketProvider } from "./context/SocketContext.jsx";
 
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <SocketProvider>
     <App />
     <Toaster closeButton />
+    <Analytics />
   </SocketProvider>
 );
