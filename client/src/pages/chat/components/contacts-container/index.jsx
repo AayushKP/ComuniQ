24dc48797030d600bf50064960216c8e385eb5ae
Ayush Kashyap <ayushprokashyap@gmail.com>
2025-03-07 19:14:04 +0530

    vh fixed

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -54,7 +54,7 @@ function ContactsContainer() {
   };
 
   return (
-    <div className="relative md:w-[30vw] bg-gradient-to-b from-gray-800 to-gray-950 lg:w-[25vw] xl:w-[20vw] border-r-2 border-[#2f303b] w-full flex flex-col h-screen">
+    <div className="relative md:w-[30vw] bg-gradient-to-b from-gray-800 to-gray-950 lg:w-[25vw] xl:w-[20vw] border-r-2 border-[#2f303b] w-full flex flex-col h-[100dvh]">
       {/* Logo Section */}
       <div className="pt-3">
         <Logo />
